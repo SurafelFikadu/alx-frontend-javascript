08135b092a7e5182dab7976ff8a3fc44ec340068
SurafelFikadu <surafelfikadu05@gmail.com>
2024-06-27 14:02:47 +0000

    ...

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,14 +1,19 @@
-import { uploadPhoto, createUser } from './util';
+// Await and async
+import { uploadPhoto, createUser } from './utils';
 
 const asyncUploadUser = async () => {
-  try {
-    const photo = await uploadPhoto();
-    const user = await createUser();
+  let photo;
+  let user;
 
-    return { photo, user };
-  } catch (error) {
-    return { photo: null, user: null };
+  try {
+    photo = await uploadPhoto();
+    user = await createUser();
+  } catch (e) {
+    photo = null;
+    user = null;
   }
+
+  return ({ photo, user });
 };
 
 export default asyncUploadUser;
